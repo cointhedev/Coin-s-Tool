a9d42b1ad0b910126ad4a16470842e69656d6fa5
cointhedev <154434854+cointhedev@users.noreply.github.com>
2023-12-30 16:03:05 +0200

    better code

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
-import sys
 import os
 import time
-os.system("title (CoinsTool) Menu")
+import sys
+
 black = "\033[1;30m"
 red = "\033[1;31m"    
 green = "\033[1;32m"
@@ -10,56 +10,6 @@ blue = "\033[1;34m"
 purple = "\033[1;35m"    
 cyan = "\033[1;36m"    
 white = "\033[1;37m"
-
-def menu():
-    os.system("cls")
-    print(f"""{red}
- $$$$$$\            $$\           $$\           
-$$  __$$\           \__|          $  |          
-$$ /  \__| $$$$$$\  $$\ $$$$$$$\  \_/  $$$$$$$\ 
-$$ |      $$  __$$\ $$ |$$  __$$\     $$  _____|
-$$ |      $$ /  $$ |$$ |$$ |  $$ |    \$$$$$$\  
-$$ |  $$\ $$ |  $$ |$$ |$$ |  $$ |     \____$$\ 
-\$$$$$$  |\$$$$$$  |$$ |$$ |  $$ |    $$$$$$$  |
- \______/  \______/ \__|\__|  \__|    \_______/ 
-$$$$$$$$\                     $$\               
-\__$$  __|                    $$ |              
-   $$ |    $$$$$$\   $$$$$$\  $$ |              
-   $$ |   $$  __$$\ $$  __$$\ $$ |              
-   $$ |   $$ /  $$ |$$ /  $$ |$$ |              
-   $$ |   $$ |  $$ |$$ |  $$ |$$ |              
-   $$ |   \$$$$$$  |\$$$$$$  |$$ |              
-   \__|    \______/  \______/ \__|              
-{blue}
-      1. Coin's Nuker
-      2. Coin's Selfbot
-      3. Coin's Nitro Generator (promocodes)
-{cyan}      4. Quit{white}
-      """)
-    try:
-        opt = int(input())
-    except Exception as e:
-        if type(e)==ValueError:
-            print("Please enter a number!")
-            time.sleep(.5)
-            opt=""
-            pass
-            menu()
-        else:
-            print(e)
-            opt=""
-            time.sleep(.5)
-            pass
-            menu()
-    if( opt!= 1 and opt!=2 and opt!=3 and opt !=4):
-        print("Please enter a valid number!")
-    if( opt!= 1 and opt!=2 and opt!=3):
-        print("Please enter a valid option!")
-        time.sleep(.5)
-        opt=""
-        menu()
-    return opt
-
 def nuker():
     os.system("py nuker.py || python3 nuker.py || python nuker.py")
 
@@ -68,11 +18,52 @@ def sb():
 
 def nitro():
     os.system("py nitro.py || python3 nitro.py || python nireo.py")
+def menu():
+    while True:
+        os.system("cls")
+        print(f"""{red}
+    $$$$$$\            $$\           $$\           
+    $$  __$$\           \__|          $  |          
+    $$ /  \__| $$$$$$\  $$\ $$$$$$$\  \_/  $$$$$$$\ 
+    $$ |      $$  __$$\ $$ |$$  __$$\     $$  _____|
+    $$ |      $$ /  $$ |$$ |$$ |  $$ |    \$$$$$$\  
+    $$ |  $$\ $$ |  $$ |$$ |$$ |  $$ |     \____$$\ 
+    \$$$$$$  |\$$$$$$  |$$ |$$ |  $$ |    $$$$$$$  |
+    \______/  \______/ \__|\__|  \__|    \_______/ 
+    $$$$$$$$\                     $$\               
+    \__$$  __|                    $$ |              
+    $$ |    $$$$$$\   $$$$$$\  $$ |              
+    $$ |   $$  __$$\ $$  __$$\ $$ |              
+    $$ |   $$ /  $$ |$$ /  $$ |$$ |              
+    $$ |   $$ |  $$ |$$ |  $$ |$$ |              
+    $$ |   \$$$$$$  |\$$$$$$  |$$ |              
+    \__|    \______/  \______/ \__|              
+    {blue}
+        1. Coin's Nuker{white}
+{cyan}        2. Coin's Selfbot{white}
+{blue}        3. Coin's Nitro Generator (promocodes){white}
+{cyan}        4. Quit{white}
+        """)
+        try:
+            opt = int(input())
+        except Exception as e:
+            if type(e)==ValueError:
+                print(f"{red}Please enter a number!{white}")
+                time.sleep(.5)
+                continue
+            else:
+                print(red+str(e)+white)
+                time.sleep(.5)
+                continue
+        if opt!= 1 and opt!=2 and opt!=3 and opt !=4:
+            print(f"{red}Please enter a valid option!{white}")
+            time.sleep(.5)
+            continue
+        else:
+            break
+    return opt
 
 option = menu()
-while option=="":
-    option = menu()
-
 if option == 1:
     nuker()
 if option == 2:
