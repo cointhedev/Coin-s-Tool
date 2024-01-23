fff4c66a1155b714e3f537df858992534ca2c464
cointhedev <154434854+cointhedev@users.noreply.github.com>
2024-01-23 10:24:03 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 echo installing libraries...
 pip install -r requirements.txt >nul || py -m pip install -r requirements.txt >nul || python -m pip install -r requirements.txt >nul || python3 -m pip install -r requirements.txt >nul
 cls
-echo Launching Coln's Tool, Please Wait.
+echo Launching Coin's Tool, Please Wait.
 timeout /t 5 /NOBREAK >nul
 py run.py || python run.py || python3 run.py
 pause
